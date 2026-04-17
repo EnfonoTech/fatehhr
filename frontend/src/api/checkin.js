cd67086b6b5170421974eebf9ae32f06951d6fda
sayanthns <sayanth2k16@gmail.com>
2026-04-18 02:06:35 +0300

    chore: gitignore + remove transpiled js/d.ts artifacts

--- frontend/src/api/checkin.js
+++
@@ -1,5 +0,0 @@
-import { apiCall } from "./client";
-export const checkinApi = {
-    create: (p) => apiCall("POST", "fatehhr.api.checkin.create", p),
-    list: (p) => apiCall("POST", "fatehhr.api.checkin.list_mine", p),
-};
--- frontend/src/api/me.js
+++
@@ -1,5 +0,0 @@
-import { apiCall } from "./client";
-export const meApi = {
-    profile: () => apiCall("GET", "fatehhr.api.me.profile"),
-    updateProfile: (patch) => apiCall("POST", "fatehhr.api.me.update_profile", patch),
-};
